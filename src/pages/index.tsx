21f5342c27f9b5716ef03c71d2d6c29f52bc5ea9
Ramon Peres <r.peres@xsensors.ai>
2022-03-11 18:16:14 -0300

    fix error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import { SectionHome } from "../components/SectionHome";
-import SectionHowItWorks from "../components/SectionHowItWorks";
 //Components
+import { SectionHome } from '../components/SectionHome';
+import { SectionHowItWorks } from '../components/SectionHowItWorks';
 import { SectionWhyConquerPage } from '../components/SectionWhyConquerPage';
-import { SectionDepositions } from "../components/SectionDepositions";
+import { SectionDepositions } from '../components/SectionDepositions';
 
 export default function Home() {
   return (
