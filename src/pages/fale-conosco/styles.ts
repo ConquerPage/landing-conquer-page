80a77c652e93dc9d844846f0381044a2b7aefb60
lucas <l.dallier@xsensors.ai>
2022-03-15 16:39:13 -0300

    Implement Fale Conosco

--- src/pages/fale-conosco/styles.ts
+++ src/pages/fale-conosco/styles.ts
@@ -175,7 +175,7 @@ export const FormContactUs = styled.form`
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
     font-weight: 700;
-    color: ${({ theme }) => theme.body};
+    color: ${allColors.color9};
 
     > svg {
       font-size: 1rem;
