7712ed64c9b7a5891b2ba9431cd045e311d92e5c
Ramon Peres <r.peres@xsensors.ai>
2022-03-22 15:28:44 -0300

    config analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
-// import * as gtag from '../lib/gtag';
+import * as gtag from '../lib/gtag';
 //SEO
 import { DefaultSeo } from 'next-seo';
 import { SEO } from '../../next-seo-config';
@@ -21,6 +21,7 @@ import { GlobalStyle } from '../styles/global';
 import { ThemeProvider } from 'styled-components';
 import { darkTheme, lightTheme } from '../styles/themes';
 import { useTheme } from '../hooks/useThemes';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { theme, toggleTheme } = useTheme();
@@ -28,15 +29,15 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   const router = useRouter();
 
-  // useEffect(() => {
-  //   const handleRouteChange = (url) => {
-  //     gtag.pageview(url);
-  //   };
-  //   router.events.on('routeChangeComplete', handleRouteChange);
-  //   return () => {
-  //     router.events.off('routeChangeComplete', handleRouteChange);
-  //   };
-  // }, [router.events]);
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   const hiddenHeaderIn = useMemo(() => ['/fale-conosco'], []);
 
@@ -52,6 +53,24 @@ function MyApp({ Component, pageProps }: AppProps) {
             <HeaderResponsive handleToogleTheme={toggleTheme} />
           ))}
         <Component {...pageProps} />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
         {/* <Analytics /> */}
       </ThemeProvider>
     </>
